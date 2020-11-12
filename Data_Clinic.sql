8ad8f5c28c1dfb03fd30fb2f73e2a865337196c6
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-12 16:39:20 -0500

    updated data

--- Data_Clinic.sql
+++ Data_Clinic.sql
@@ -15,11 +15,11 @@ VALUES
        ( "Dermatología" ), ( "Ginecología" ) ,
        ( "Oftalmología" ), ( "Traumatología" ) ;
 
-INSERT INTO tb_doctor ( name, last_name, id_hour, id_speciality )
+INSERT INTO tb_doctor ( name, a_p, a_m, dni, password, id_hour, id_speciality )
 VALUES
-       ( "William", "Osler", 2, 1 ) ,
-       ( "Ignaz", "Semmelweis", 1, 3 ) ,
-       ( "Sigmund", "Freud", 1, 2 ) ;
+       ( "William", "Osler", "Semmelweis", "78965411", "password", 2, 1 ) ,
+       ( "Ignaz", "Semmelweis", "Osler", "78965412", "password", 1, 3 ) ,
+       ( "Sigmund", "Freud", "Ignaz", "78965413", "password", 1, 2 ) ;
 
 INSERT INTO tb_gender ( gender )
 VALUES
@@ -29,36 +29,74 @@ INSERT INTO tb_type_identification ( type )
 VALUES
        ( "DNI" ), ( "Pasaporte" ), ( "Carnet de extrangeria" ) ;
 
-INSERT INTO tb_patient ( name, last_name, phone, email, identification, id_gender, id_type_identification )
+INSERT INTO tb_marital_status ( marital_status )
 VALUES
-       ( "Alexandra Jaqueline", "Navarro Navarro", "999999999", null, "78965412", 2, 1 ) ,
-       ( "Leonardo Antonio", "Nolasco Leyva", "996501260", "leo2001.nl8@gmail.com", "71062234", 1, 1 ) ;
+       ( "Casado" ), ( "Divorciado" ), ("Viudo"), ("Soltero") ;
 
-INSERT INTO tb_clinic_history ( id_patient )
+INSERT INTO tb_clinic_history ( id )
 VALUES
-       ( 1 );
+       ( 1 ), ( 2 ), ( 3 );
 
-INSERT INTO tb_diagnosis ( id_doctor, disease, id_history_clinic )
+INSERT INTO tb_health_insurance ( name )
 VALUES
-       ( 2, "Gripe", 1 );
+       ( "ESSALUD" ), ( "SIS" );
 
-INSERT INTO tb_receptionist ( name, last_name, dni )
+INSERT INTO tb_district ( district )
 VALUES
-       ( "Wichai", "Thongthan", "12345678" ) ,
-       ( "Vernon", "Jordan", "96587412" ) ;
+       ( "San Juan de Miraflores" ), ( "Chorrillos" );
 
-INSERT INTO tb_contrareferencia ( id_receptionist )
+INSERT INTO tb_patient (
+                        id, name, a_p, a_m, phone, birthdate,
+                        address, hopital_perteneciente, id_type_identification,
+                        id_gender, id_marital_status, id_clinic_history,
+                        id_health_insurance, id_district )
 VALUES
-       ( 2 ) ;
+       ( "71062231", "Alexandra Jaqueline", "Navarro", "Navarro", "999999999", "11-11-20" ,
+        "Hopital", "nombre de hostipal" ,
+        2, 1, 4, 2 ,
+        2, 1 ) ,
+       ( "71062232", "Leonardo Antonio", "Nolasco", "Leyva", "996501260", "11-11-20" ,
+        "San Genaro", "San Jose" ,
+        1, 1, 4, 1 ,
+        1, 2 ) ,
+       ( "71062233", "Chester", "Mufin", "Cmcut", "859674123", "11-11-20" ,
+        "Pamplona", "Oyantai" ,
+        1, 1, 3, 3 ,
+        1, 1 ) ;
 
-INSERT INTO tb_appointment ( appointment_time, id_patient, id_doctor, id_receptionist )
+INSERT INTO tb_diagnosis (
+                          reasons_for_consultation, current_illness,
+                          pathological_history, id_clinic_history,
+                          id_doctor )
 VALUES
-        ( "08:30:00", 1, 2, 1 ) ,
-        ( "16:30:00", 2, 1, 2 ) ;
+       ( "Malestar general", null, null, 2, 1 ) ,
+       ( "Gripe", null, null, 1, 2 ) ;
 
-INSERT INTO tb_cashier ( name, last_name, dni )
+INSERT INTO tb_receptionist ( name, a_p, a_m, dni, password )
 VALUES
-        ( "Almirante Miguel", "Grau Seminario", "12344321" ) ;
+       ( "Wichai", "Thongthan", "Vernon", "12345678", "12345678" ) ,
+       ( "Vernon", "Jordan", "Thongthan", "96587412", "74125896" ) ;
+
+INSERT INTO tb_contrareferencia (
+                                 est_ref_name, est_ref_dir, est_contref_name,
+                                 est_contref_dir, est_contref_diagnosis, treatment,
+                                 ind_aps, date_contref,
+                                 id_patient,
+                                 id_doctor, id_receptionist )
+VALUES
+       ( "nombre de posta", "direccion posta", "Maria Auxiliadora" ,
+        "San Juan de Miraflores", "diagnostico", "tratamiento", "indicaciones de APS", "10-10-20",
+        "71062233",
+        1, 1) ;
+
+INSERT INTO tb_appointment ( hour, date, id_doctor, id_patient, id_receptionist )
+VALUES
+        ( "08:30:00", "10-10-20", 1, "71062232", 2 ) ,
+        ( "16:30:00", "10-10-20", 2, "71062231", 1 ) ;
+
+INSERT INTO tb_cashier ( name, a_p, a_m, dni, password )
+VALUES
+        ( "Almirante Miguel", "Grau" ,"Seminario", "12344321", "password") ;
 
 INSERT INTO tb_proof_of_purchase ( id_appointment, id_cashier )
 VALUES
